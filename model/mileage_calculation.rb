f83222b84876a97b5aadc02f5618c70678cc2998
DON BADSHA <DON@letsgors.com>
2024-12-05 00:35:24 -0500

    Update mileage_calculation.rb

--- model/mileage_calculation.rb
+++ model/mileage_calculation.rb
@@ -2,13 +2,33 @@ class MileageCalculation
   attr_accessor :start_location, :end_location
 
   def initialize(start_location, end_location)
+    raise ArgumentError, 'start_location cannot be nil' if start_location.nil?
+    raise ArgumentError, 'end_location cannot be nil' if end_location.nil?
+
     @start_location = start_location
     @end_location = end_location
   end
 
   def calculate_mileage
-    distance = rand(5..20)
+    begin
+      distance = calculate_distance(@start_location, @end_location)
+      log_calculation(distance)
+      distance
+    rescue => e
+      puts "An error occurred: #{e.message}"
+      nil
+    end
+  end
+
+  private
+
+  def calculate_distance(start_location, end_location)
+    # Replace with actual mileage calculation logic
+    rand(5..20)
+  end
+
+  def log_calculation(distance)
+    # Replace with actual logging mechanism
     puts "Calculated distance: #{distance} miles"
-    distance
   end
 end
