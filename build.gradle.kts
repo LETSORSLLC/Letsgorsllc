2c57c2eefecc0430c22700b8a858b838b52a01b3
DON BADSHA <DON@letsgors.com>
2024-12-10 12:33:39 -0500

    Update build.gradle.kts
    Signed-off-by: DON BADSHA <DON@letsgors.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application") version "7.3.1"
     id("org.jetbrains.kotlin.android") version "1.7.20"
+    id("com.android.dynamic-feature") // Add dynamic-feature plugin
 }
 
 android {
