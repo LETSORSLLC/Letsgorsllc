02bcdc6f704341f1a39e6a49b8f438dc20e36e06
DON BADSHA <DON@letsgors.com>
2024-12-13 08:40:38 -0500

    updated
    making it right

---
+++ Let's Go/Let's GoTests/Let_s_GoTests.swift
@@ -0,0 +1,16 @@
+//
+//  Let_s_GoTests.swift
+//  Let's GoTests
+//
+//  Created by Don Badsha on 11/29/24.
+//
+
+import Testing
+
+struct Let_s_GoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
